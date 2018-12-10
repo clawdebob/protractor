9e095b215a30df04c5acd221a3949b696db73d95
clawdebob <mok938@gmail.com>
2018-12-10 16:58:22 +0300

    added webhelp test

--- hintconf.js
+++ hintconf.js
@@ -1,20 +1,12 @@
 exports.config = {
-//  seleniumAddress: 'http://localhost:4444/wd/hub',
   getPageTimeout: 30000,
   allScriptsTimeout: 30000,
   framework: 'jasmine2',
   jasmineNodeOpts: { defaultTimeoutInterval: 30000 },
-  /*onPrepare: function() {
-     // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
-     jasmine.getEnv().addReporter(new HtmlReporter({
-        baseDirectory: './reports/screenshots'
-     }).getJasmine2Reporter());
-  },*/
   rootElement: 'keenetic',
   capabilities: {
     browserName: 'chrome',
     chromeOptions: { args: [
-      //"lang=en-EN",
       //"--headless",
       //"--disable-gpu",
       "--window-size=800,600",
@@ -24,29 +16,30 @@ exports.config = {
   specs: ['hints_test.js'],
   onPrepare: function(){
     browser.get('http://192.168.1.1');
+    browser.driver.manage().deleteAllCookies();
     var until = protractor.ExpectedConditions;
     var login = element(by.xpath('//input[@name="loginLogin"]'));
     var password = element(by.xpath('//input[@name="loginPassword"]'));
     var submit = element(by.tagName('button'));
     var lang = element(by.xpath('//div[@id="sb-1"]/a'));
     browser.driver.wait(function () {
-      browser.wait(until.visibilityOf(login), 10000);
+      browser.wait(until.visibilityOf(login), 10000,"login input field wasn't found");
       return login;
     }).then(function(){
       login.clear().sendKeys('admin');
     });
     browser.driver.wait(function () {
-      browser.wait(until.visibilityOf(password), 10000);
+      browser.wait(until.visibilityOf(password), 10000,"password input field wasn't found");
       return password;
     }).then(function(){
       password.clear().sendKeys('1');
     });
     browser.driver.wait(function () {
-      browser.wait(until.visibilityOf(lang), 10000);
+      browser.wait(until.visibilityOf(lang), 10000, "language select menu wasn't found");
       return lang;
     }).then(function(el){
       el.click();
-      element(by.linkText('English')).click()
+      element(by.linkText('Русский')).click()
     });
     submit.click();
     return browser.driver.wait(function() {
--- hints_test.js
+++ hints_test.js
@@ -2,28 +2,60 @@ describe("Hints testing", function(){
   var until = protractor.ExpectedConditions;
   it("surfing to hint",function(){
     var menu_button = element(by.id('ndm_menu_toggle'));
-    var option = element(by.linkText('Wi-Fi system'));
-    //var option = element(by.linkText('Wi-Fi-система'));
+    var option = element(by.linkText('Проводной'));
     browser.driver.wait(function () {
-      browser.wait(until.visibilityOf(menu_button), 10000);
+      browser.wait(until.visibilityOf(menu_button), 10000,"menu toggle button wasn't found");
       return menu_button;
     }).then(function(el){
       el.click();
     });
     browser.driver.wait(function () {
-      browser.wait(until.visibilityOf(option), 10000);
+      browser.wait(until.visibilityOf(option), 10000,"option wasn't found");
       return option;
     }).then(function(el){
       el.click();
     });
-    var hint = element(by.tagName('ndm-help'))
+  });
+  it("checking hint without internet", function(){
+    var toggleb = element(by.xpath('//label[@for="up__0toggle"]'));
+    var hint = element.all(by.tagName('ndm-help')).get(0);
+    var txt_on = element(by.cssContainingText('.ndm-toggle .toggle-text .toggle-description.help-text--success', 'Подключено'));
+    var txt_off = element(by.cssContainingText('.ndm-toggle .toggle-text .toggle-description', 'Выключен'));
+    var links = element.all(by.repeater('note in vm.notes'));
     browser.driver.wait(function () {
-      browser.wait(until.visibilityOf(hint), 10000);
+      browser.wait(until.visibilityOf(txt_on), 10000,"text confirming succesful connection wasn't found");
+      return toggleb;
+    }).then(function(el){
+      el.click();
+      browser.wait(until.visibilityOf(txt_off), 10000,"text confirming abscence of connection wasn't found");
+    }).then(function(){
+      browser.driver.wait(function () {
+        browser.wait(until.visibilityOf(hint), 10000, "hint wasn't found");
+        return hint;
+      }).then(function(el){
+          browser.actions().mouseMove(el).perform();
+          browser.wait(until.stalenessOf(links.get(0)), 10000,"links are not abscent");
+      });
+    }).then(function(){
+      browser.driver.wait(function () {
+        return toggleb;
+      }).then(function(el){
+        el.click();
+        browser.wait(until.visibilityOf(txt_on), 10000,"text confirming succesful connection wasn't found");
+      });
+    });
+  });
+  it("checking hint with internet connection",function(){
+    var hint = element.all(by.tagName('ndm-help')).get(0);
+    var links = element.all(by.repeater('note in vm.notes'));
+    browser.refresh();
+    browser.driver.wait(function () {
+      browser.wait(until.visibilityOf(hint), 10000,"hint wasn't found");
       return hint;
     }).then(function(el){
         browser.actions().mouseMove(el).perform();
+        browser.wait(until.visibilityOf(links.get(0)),20000,"links from database weren't found");
+        expect(links.count()).not.toBeLessThan(1);
     });
-    //browser.actions().mouseMove(element(by.id('sb-1'))).perform();
-    browser.sleep(4000);
   });
 });
